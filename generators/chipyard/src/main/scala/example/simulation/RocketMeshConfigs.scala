dedd561b85fa85f62a996bd70147367a3613dd94
Mahyar Emami <mayyxeng@gmail.com>
2023-11-10 13:34:52 +0100

    fix mesh configs

--- generators/chipyard/src/main/scala/example/simulation/RocketMeshConfigs.scala
+++ generators/chipyard/src/main/scala/example/simulation/RocketMeshConfigs.scala
@@ -36,7 +36,7 @@ class WithMultiMeshNoC(nX: Int, nY: Int, mX: Int, mY: Int) extends Config({
             HierarchicalTopology(
                 base = Mesh2D(mX, mY),
                 children = Seq.tabulate(mX * mY) { s =>
-                    HierarchicalSubTopology(s, 0, Mesh2D(nX, nY))
+                    HierarchicalSubTopology(s, nX * nY - 1, Mesh2D(nX, nY))
                 }
             )
         )
@@ -74,6 +74,7 @@ class WithMultiMeshNoC(nX: Int, nY: Int, mX: Int, mY: Int) extends Config({
             skipValidationChecks = false, // takes forever to check
         )
     val nSockets = mX * mY
+    require(nSockets >= 2, "at least 2 sockets needed")
     val coresPerSocket = nX * nY
     val coresTotal = mX * mY * coresPerSocket
     val coreMapping: ListMap[String, Int] = ListMap.from(List.tabulate(nSockets) { s =>
@@ -88,8 +89,8 @@ class WithMultiMeshNoC(nX: Int, nY: Int, mX: Int, mY: Int) extends Config({
                 inNodeMapping = coreMapping,
                 outNodeMapping = ListMap(
                     "pbus" -> 0,
-                    s"system[0]" -> 0, // MEM
-                    s"system[1]" -> 0 // MMIO
+                    s"system[0]" -> 1, // MEM
+                    s"system[1]" -> 1 // MMIO
                 )
             ),
             nocParams = nocParams
@@ -101,7 +102,7 @@ class WithMultiMeshNoC(nX: Int, nY: Int, mX: Int, mY: Int) extends Config({
 })
 
 class AnySmallRocketMultiMeshConfig(nX: Int, nY: Int, mX: Int, mY: Int) extends Config(
-    new freechips.rocketchip.subsystem.WithNSmallCores(n = 2 * nX * nY) ++
+    new freechips.rocketchip.subsystem.WithNSmallCores(n = mX * mY * nX * nY) ++
     new WithMultiMeshNoC(nX, nY, mX, mY)
 )
 
@@ -130,5 +131,38 @@ class SmallRocket7x7Core2x2MeshConfig extends AnySmallRocketMultiMeshConfig(7, 7
 class SmallRocket8x8Core2x2MeshConfig extends AnySmallRocketMultiMeshConfig(8, 8, 2, 2)
 class SmallRocket9x9Core2x2MeshConfig extends AnySmallRocketMultiMeshConfig(9, 9, 2, 2)
 class SmallRocket10x10Core2x2MeshConfig extends AnySmallRocketMultiMeshConfig(10, 10, 2, 2)
+
+
+
+class AnySmallBoomMeshConfig(nX: Int, nY: Int) extends Config(
+    new boom.common.WithNSmallBooms(n = nX * nY - 3) ++
+    new WithMesh2DNoC(nX, nY)
+)
+class AnyLargeBoomMeshConfig(nX: Int, nY: Int) extends Config(
+    new boom.common.WithNLargeBooms(n = nX * nY - 3) ++
+    new WithMesh2DNoC(nX, nY)
+)
+
+
+
+class SmallBoom2x2CoreMeshConfig extends AnySmallBoomMeshConfig(2, 2)
+class SmallBoom3x3CoreMeshConfig extends AnySmallBoomMeshConfig(3, 3)
+class SmallBoom4x4CoreMeshConfig extends AnySmallBoomMeshConfig(4, 4)
+class SmallBoom5x5CoreMeshConfig extends AnySmallBoomMeshConfig(5, 5)
+class SmallBoom6x6CoreMeshConfig extends AnySmallBoomMeshConfig(6, 6)
+class SmallBoom7x7CoreMeshConfig extends AnySmallBoomMeshConfig(7, 7)
+class SmallBoom8x8CoreMeshConfig extends AnySmallBoomMeshConfig(8, 8)
+
+
+
+class LargeBoom2x2CoreMeshConfig extends AnyLargeBoomMeshConfig(2, 2)
+class LargeBoom3x3CoreMeshConfig extends AnyLargeBoomMeshConfig(3, 3)
+class LargeBoom4x4CoreMeshConfig extends AnyLargeBoomMeshConfig(4, 4)
+class LargeBoom5x5CoreMeshConfig extends AnyLargeBoomMeshConfig(5, 5)
+class LargeBoom6x6CoreMeshConfig extends AnyLargeBoomMeshConfig(6, 6)
+class LargeBoom7x7CoreMeshConfig extends AnyLargeBoomMeshConfig(7, 7)
+class LargeBoom8x8CoreMeshConfig extends AnyLargeBoomMeshConfig(8, 8)
+
+
 
 
